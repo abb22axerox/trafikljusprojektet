529788567013ad85ad8f0c7f0bad9a9c63d4a5e3
abb22axerox <axel.roxenborg@abbgymnasiet.se>
2024-01-10 13:44:44 +0100

    mapp fix

---
+++ setup/setup.ino
@@ -0,0 +1,17 @@
+//# INPUT #​
+//Arduino pins och bytedata
+
+//# OUTPUT #​
+//[]
+//Funktioner för att stänga av/på​
+//Namn på knappar
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
