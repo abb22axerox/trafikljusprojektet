fb3515f05276b91d20ef643ff78e3e3dc5fbe442
abbvighai <viggo.haimanas.bygden@abbgymnasiet.se>
2024-01-15 14:55:11 +0100

    Knappar utan setup och loop

--- setup/knappar.ino
+++ setup/knappar.ino
@@ -7,23 +7,16 @@ bool ButtonStates[] = {true, false, false, false, true}; // testkod d.v.s statio
 //# OUTPUT #​
 //Btn: int, in range 1-4
 //Walk: boolean
-int StatesLength = 5;
 int Btn;
-boolean Walk = ButtonStates[StatesLength - 1];
+boolean Walk;
 
-void setup() {
-  // put your setup code here, to run once:
+void knappar() {
+  int StatesLength = 5;
+  Walk = ButtonStates[StatesLength - 1];
   for (int i = 0; i < StatesLength - 1; i++) {
     if (ButtonStates[i]) {
       Btn = i;
-      Btn= Btn+1;
+      Btn = Btn + 1;
     }
   }
-  Serial.begin(9600);
-  Serial.println(Btn);
-  Serial.println(Walk);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
 }
