97d5941803fd13b580e26ec8184cb18f45a4cfe2
abbdanlin22 <daniel.lindvall@abbgymnasiet.se>
2024-01-16 15:29:46 +0100

    Done System and begin at TurnAdjuster

--- setup/Turnadjuster.ino
+++ setup/Turnadjuster.ino
@@ -8,12 +8,9 @@
 
 // OBS! Index är en under stationsnummer
 
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+int TurnAdjuster[][] = {
+  {2, 4, 3}
+  {4, 3, 1}
+  {1, 2, 4}
+  {3, 1, 2}
+};
