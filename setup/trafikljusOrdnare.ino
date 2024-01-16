9456676163cc7ccbd89905ccd28ea570fa5f6ced
abbvighai <viggo.haimanas.bygden@abbgymnasiet.se>
2024-01-16 10:46:35 +0100

    added some variables

--- setup/trafikljusOrdnare.ino
+++ setup/trafikljusOrdnare.ino
@@ -6,17 +6,8 @@ int Light_instructions[] = [2, 4];
 //# OUTPUT #​
 // WaitingLights: listor med Light_instructions listor exempelvis WaitingLights = [[[2,4],[0]​],...]
 // Där index 0 är de nästa trafikljusen som aktiveras
-int WaitingLights
+int WaitingLights[] = [[7,2]]
+
 void trafikljusOrdnare() {
-
-}
-
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
 
 }
