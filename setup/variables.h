e4e5211f14c16656438418ab06a844ea0a2f29b1
abbvighai <viggo.haimanas.bygden@abbgymnasiet.se>
2024-01-16 15:02:24 +0100

    added variables.h and removed exempelkod

---
+++ setup/variables.h
@@ -0,0 +1,13 @@
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+int Btn;
+bool Walk;
+void knappar();
+int Turn;
+bool ButtonStates[];
+void LightState();
+int TurnAdjuster[];
+int Light_instructions[];
+int WaitingLights[];
+#endif // VARIABLES_H
