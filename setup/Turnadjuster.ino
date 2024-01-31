098c46e7c0974f97e4885298ea725b099424ddd9
abbvighai <viggo.haimanas.bygden@abbgymnasiet.se>
2024-01-31 15:00:20 +0100

    added comments for next lesson

--- setup/Turnadjuster.ino
+++ setup/Turnadjuster.ino
@@ -14,3 +14,5 @@ int TurnAdjuster[][3] = {
   {0, 1, 3},
   {2, 0, 1}
 };
+
+// 0 reagerar inte vid index 2
--- setup/trafikljusOrdnare.ino
+++ setup/trafikljusOrdnare.ino
@@ -3,17 +3,25 @@
 // Light_instructions: [2,4] de som ska bli gröna
 #include "variables.h"
 
+unsigned long lastActivationMillis;
+unsigned long difference_in_millis = 0;
+
 void addToQue(int firstLight, int secondLight) {
   if (prevBtn != Btn) {
+    lastActivationMillis = millis();
     Serial.println(firstLight);
-    StartLightCycle(firstLight, millis());
+    StartLightCycle(firstLight, lastActivationMillis);
+    if (secondLight > 0 && secondLight < 8) {
+      StartLightCycle(secondLight, lastActivationMillis);
+      Serial.print("secondLight:");
+      Serial.println(secondLight);
+    }
     prevBtn = Btn;
+  } else if (difference_in_millis > 8000){
+    prevBtn = -1;
+    Btn = -1; // då stämmer likheten än vilket leder till att om Btn ändrar värde nu så körs en ny cycle och om igen
   }
-  // if (secondLight > 0 && secondLight < 8) {
-  //   StartLightCycle(secondLight, millis());
-  //   Serial.print("secondLight:");
-  //   Serial.println(secondLight);
-  // }
+  difference_in_millis = millis() - lastActivationMillis; // kör om cycle direkt efter hitta ett sätt att identifiera om knappen redan körde nyss, problem nu station 2 återupprepas
   // int* newInstructions; // definiera de nya instruktionerna
   // int** newArray = new int*[waitingLightsLength + 1];
 
